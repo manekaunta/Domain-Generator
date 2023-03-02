f30eef330694404eec27fcfa91d69613642606da
manekaunta <manekaunta@gmail.com>
2023-03-02 19:31:08 +0000

    Domain Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["his", "we"];
+  let adj = ["fun", "brilliant"];
+  let noun = ["game", "creators"];
+  let website = [".com", ".org", ".net"];
+  let domainList = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < website.length; l++) {
+          domainList.push(pronoun[i] + adj[j] + noun[k] + website[l]);
+        }
+      }
+    }
+  }
+  domainList.sort();
+  let element = document.querySelector(".domain-list");
+  for (let i = 0; i < domainList.length; i++) {
+    element.innerHTML += "<li>" + domainList[i] + "</li>";
+  }
 };
